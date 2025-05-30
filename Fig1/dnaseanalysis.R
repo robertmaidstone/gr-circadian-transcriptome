278f0db39fb4a4e848e67e2d8e6c52dbef69fae7
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2025-05-30 14:46:38 +0100

    dnase analysis

---
+++ Fig1/dnaseanalysis.R
@@ -0,0 +1,39 @@
+
+library(GenomicRanges)
+library(PopSV)
+library(tidyverse)
+
+load("~/GRanalysis_master/Fig2/data/enhancer_master.RData")
+
+data_master %>% dplyr::select(ENHANCERID) %>% separate(ENHANCERID,into = c("chr","start","end")) %>% unique %>%
+  mutate(start=as.numeric(start)) %>% mutate(end=as.numeric(end))-> enh_regions
+
+setwd("~/GRanalysis_master/ABC/")
+DNase_counts<-bin.bw("~/GRanalysis_master/ABC/GSM1479701_WT_DNAse_ZT02.bw", enh_regions,appendIndex.outfile = FALSE)
+
+DNase_counts %>% head
+
+DNase_counts$bc %>% mutate(ENHANCERID=paste(chr,start,end,sep=".")) %>% merge(data_master,by="ENHANCERID") -> ll_enh
+
+ll_enh %>% mutate(GR=(GRlou_dex_us==0)|(GRlou_WT_us==0)) %>% dplyr::select(ENHANCERID,bc,GR) %>% unique %>% as_tibble %>%
+  ggplot(aes(x=bc,fill=GR)) + geom_density(alpha=0.4) + scale_x_log10(lim=c(0.08,10)) + scale_fill_manual(values = c("black","red")) +
+  theme_bw() + theme(legend.position = "none") + ylab("Density") + xlab("Normalised Counts") -> p_enhancers
+
+### promoters
+
+load("~/GRanalysis_master/GR_CircadianLiverTranscriptome/Data/RNAChIPtogether_GRdist.RData")
+
+data_master %>% dplyr::select(PROMID) %>% separate(PROMID,into = c("chr","start","end")) %>% unique %>%
+  mutate(start=as.numeric(start)) %>% mutate(end=as.numeric(end))-> prom_regions
+
+prom_regions %>% filter(!is.na(start)) -> prom_regions
+
+DNase_counts<-bin.bw("~/GRanalysis_master/ABC/GSM1479701_WT_DNAse_ZT02.bw", prom_regions,appendIndex.outfile = FALSE)
+
+DNase_counts %>% head
+
+DNase_counts$bc %>% mutate(PROMID=paste(chr,start,end,sep=".")) %>% merge(data_master,by="PROMID") -> ll_prom
+
+ll_prom %>% mutate(GR=(GRlou_dex_us==0)|(GRlou_WT_us==0)) %>% dplyr::select(PROMID,bc,GR) %>% unique %>% as_tibble %>%
+  ggplot(aes(x=bc,fill=GR)) + geom_density(alpha=0.4) + scale_x_log10(lim=c(0.08,10)) + scale_fill_manual(values = c("black","red")) + 
+  theme_bw() + theme(legend.position = "none") + ylab("Density") + xlab("Normalised Counts") -> p_promoters
--- Fig2/comparisonbetweenCHICandnearest.R
+++ Fig2/comparisonbetweenCHICandnearest.R
@@ -26,6 +26,17 @@ data_master_all %>%
 
 data_master_all %>% 
   mutate(JTK_pvalue=JTK_pvalue.x,JTK_adjphase=JTK_adjphase.x) %>%
+  dplyr::select(GeneID,ENHANCERID,WT_us=(GRlou_WT_us),Dex_us=GRlou_dex_us,JTK_pvalue,JTK_adjphase) %>%
+  unique %>%
+  mutate(Both_us=(Dex_us==0),Rhythmic=JTK_pvalue<0.05) %>% 
+  dplyr::select(GeneID,Rhythmic,Both_us,JTK_adjphase) %>% 
+  as_tibble %>% group_by(GeneID) %>% dplyr::mutate(Rhythmic.g=any(Rhythmic),Both_us.g=any(Both_us)) %>% 
+  ungroup %>%
+  dplyr::select(GeneID,Rhythmic.g,Both_us.g,JTK_adjphase) %>%
+  unique  -> chic_all
+
+data_master_all %>% 
+  mutate(JTK_pvalue=JTK_pvalue.x,JTK_adjphase=JTK_adjphase.x) %>%
   dplyr::select(PROMID,To,ENHANCERID,WT_us=(GRlou_WT_us),Dex_us=GRlou_dex_us,JTK_pvalue,JTK_adjphase) %>%
   unique %>%
   mutate(Both_us=(Dex_us==0),Rhythmic=JTK_pvalue<0.05) %>% 
@@ -50,7 +61,18 @@ data_master_all %>%
   filter(Rhythmic.g==TRUE) %>%
   filter(Both_us.g==FALSE) -> nearest_TF
 
-save(chic_TT,chic_TF,nearest_TT,nearest_TF,file="Data/genelistsforcomparisonbetweenCHICandnearest.RData")
+data_master_all %>% 
+  mutate(JTK_pvalue=JTK_pvalue.x,JTK_adjphase=JTK_adjphase.x) %>%
+  dplyr::select(PROMID,To,ENHANCERID,WT_us=(GRlou_WT_us),Dex_us=GRlou_dex_us,JTK_pvalue,JTK_adjphase) %>%
+  unique %>%
+  mutate(Both_us=(Dex_us==0),Rhythmic=JTK_pvalue<0.05) %>% 
+  dplyr::select(To,Rhythmic,Both_us,JTK_adjphase) %>% 
+  as_tibble %>% group_by(To) %>% dplyr::mutate(Rhythmic.g=any(Rhythmic),Both_us.g=any(Both_us)) %>% 
+  ungroup %>%
+  dplyr::select(To,Rhythmic.g,Both_us.g,JTK_adjphase) %>%
+  unique -> nearest_all
+
+save(chic_TT,chic_TF,chic_all,nearest_TT,nearest_TF,nearest_all,file="Data/genelistsforcomparisonbetweenCHICandnearest.RData")
 
 library(VennDiagram)
 venn.plot <- draw.pairwise.venn(
@@ -74,6 +96,16 @@ venn.plot <- draw.pairwise.venn(
   length(c(nearest_TF$To,nearest_TT$To)), 
   length(c(chic_TF$GeneID,chic_TT$GeneID)), 
   length(intersect(c(nearest_TF$To,nearest_TT$To), c(chic_TF$GeneID,chic_TT$GeneID))),
+  category = c("nearest", "chic"),
+  fill = c("blue", "red"),
+  alpha = 0.5
+)
+
+
+venn.plot <- draw.pairwise.venn(
+  length(c(nearest_all$To)), 
+  length(c(chic_all$GeneID)), 
+  length(intersect(c(nearest_all$To), c(chic_all$GeneID))),
   category = c("nearest", "chic"),
   fill = c("blue", "red"),
   alpha = 0.5
