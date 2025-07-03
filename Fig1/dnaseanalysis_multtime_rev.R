a93c98188763a58ca4e09da9e3e8af6066487f06
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2025-07-03 09:34:04 +0100

    change dnase

--- Fig1/dnaseanalysis_multtime_rev.R
+++ Fig1/dnaseanalysis_multtime_rev.R
@@ -4,16 +4,17 @@ library(PopSV)
 library(tidyverse)
 library(effectsize)
 
-load("~/GRanalysis_master/Fig2/data/enhancer_master.RData")
+load("~/GRanalysis_master/GR_CircadianLiverTranscriptome/Data/enhancer_data_final_rev.RData")
 
-data_master %>% dplyr::select(ENHANCERID) %>% separate(ENHANCERID,into = c("chr","start","end"),remove = FALSE) %>% unique %>%
+enh_allgenes %>% dplyr::select(ENHANCERID) %>% separate(ENHANCERID,into = c("chr","start","end"),remove = FALSE) %>% unique %>%
   mutate(start=as.numeric(start)) %>% mutate(end=as.numeric(end))-> enh_regions
 
 enh_regions <- makeGRangesFromDataFrame(enh_regions,
                                         keep.extra.columns = TRUE,
                                         seqnames.field = "chr",
                                         start.field = "start",
-                                        end.field = "end")
+                                        end.field = "end",
+                                        na.rm=TRUE)
 
 enh_regions_mm9<-as.data.frame(easyLift::easyLiftOver(enh_regions,map = "mm10_mm9"))
 enh_regions_mm9 %>% dplyr::select(chr=seqnames,start,end,ENHANCERID) -> enh_regions_mm9
@@ -27,6 +28,10 @@ DNase_counts_18<-bin.bw("GSM1479705_WT_DNAse_ZT18.bw", enh_regions_mm9,appendInd
 DNase_counts_22<-bin.bw("GSM1479706_WT_DNAse_ZT22.bw", enh_regions_mm9,appendIndex.outfile = FALSE)
 
 
+enh_allgenes %>% filter(!is.na(ENHANCERID)) %>% 
+  dplyr::select(Time,ENHANCERID,TF,Reads,GR_lou_WT,GR_lou_dex) %>%
+  unique %>% 
+  pivot_wider(names_from = TF,values_from = Reads) -> data_master
 
 enh_regions_mm9 %>% arrange(chr,start,end) %>% cbind(.,bc2=DNase_counts_2$bc$bc,
                                                      bc6=DNase_counts_6$bc$bc,
@@ -38,11 +43,11 @@ enh_regions_mm9 %>% arrange(chr,start,end) %>% cbind(.,bc2=DNase_counts_2$bc$bc,
   mutate(mean_bc = mean(c_across(bc2:bc22), na.rm = TRUE)) %>%
   ungroup() -> ll_enh
 
-ll_enh %>% mutate(GR=(GRlou_dex_us==0)|(GRlou_WT_us==0)) %>% dplyr::select(ENHANCERID,mean_bc,GR) %>% unique %>% as_tibble %>%
+ll_enh %>% as_tibble %>%mutate(GR=(GR_lou_dex==TRUE)|(GR_lou_WT==TRUE)) %>% dplyr::select(ENHANCERID,mean_bc,GR) %>% unique %>% as_tibble %>%
   ggplot(aes(x=mean_bc+.1,fill=GR)) + geom_density(alpha=0.4) + scale_x_log10(lim=c(0.08,100)) + scale_fill_manual(values = c("black","red")) +
   theme_bw() + theme(legend.position = "none") + ylab("Density") + xlab("Normalised Counts") -> p_enhancers
 
-ggsave(plot = p_enhancers,filename = "~/GRanalysis_master/GR_CircadianLiverTranscriptome/Fig2/Plots/dnase_mult_enhancers.png",width=4,height=3)
+ggsave(plot = p_enhancers,filename = "~/GRanalysis_master/GR_CircadianLiverTranscriptome/Fig2/Plots/dnase_mult_enhancers_rev.png",width=4,height=3)
 
 ### promoters
 
@@ -94,7 +99,7 @@ ggsave(plot = p_promoters,filename = "~/GRanalysis_master/GR_CircadianLiverTrans
 
 save(ll_enh,ll_prom,file = "~/GRanalysis_master/GR_CircadianLiverTranscriptome/Data/dnasedata.RData")
 
-ll_enh %>% mutate(GR=(GRlou_dex_us==0)|(GRlou_WT_us==0)) %>% 
+ll_enh %>% mutate(GR=(GR_lou_dex==TRUE)|(GR_lou_WT==TRUE)) %>% 
   group_by(ENHANCERID) %>%
   mutate(BMAL1=mean(BMAL1,na.rm=T)) %>% 
   mutate(CLOCK=mean(CLOCK,na.rm=T)) %>% 
@@ -103,8 +108,8 @@ ll_enh %>% mutate(GR=(GRlou_dex_us==0)|(GRlou_WT_us==0)) %>%
   mutate(NPAS2=mean(NPAS2,na.rm=T)) %>% 
   mutate(PER1=mean(PER1,na.rm=T)) %>% 
   mutate(PER2=mean(PER2,na.rm=T)) %>% 
-  #mutate(=mean(CLOCK,na.rm=T)) %>%  #need reverb
-  dplyr::select(ENHANCERID,bc=mean_bc,GR,BMAL1,CLOCK,CRY1,CRY2,NPAS2,PER1,PER2) %>% unique %>% as_tibble -> mod_data
+  mutate(REVERBa=mean(`REVERBa*`,na.rm=T)) %>%  #need reverb
+  dplyr::select(ENHANCERID,bc=mean_bc,GR,BMAL1,CLOCK,CRY1,CRY2,NPAS2,PER1,PER2,REVERBa) %>% unique %>% as_tibble -> mod_data
 
 lm(BMAL1 ~ bc + GR,data = mod_data) -> model_1
 standardize(model_1, method = "refit")  # standardizes predictors and outcome
@@ -131,6 +136,10 @@ standardize(model_1, method = "refit")  # standardizes predictors and outcome
 effectsize::standardize_parameters(model_1, method = "refit")
 
 lm(PER2 ~ bc + GR,data = mod_data) -> model_1
+standardize(model_1, method = "refit")  # standardizes predictors and outcome
+effectsize::standardize_parameters(model_1, method = "refit")
+
+lm(REVERBa ~ bc + GR,data = mod_data) -> model_1
 standardize(model_1, method = "refit")  # standardizes predictors and outcome
 effectsize::standardize_parameters(model_1, method = "refit")
 
